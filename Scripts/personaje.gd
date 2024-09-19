8d7626705f4f9dfc8f801408f5915f0d2fb2a34b
Thioalejo <alejo.saraza@gmail.com>
2024-09-18 23:31:58 -0500

    Se adiciona nuevo personaje y se da una animación de reposo

--- Scripts/personaje.gd
+++ Scripts/personaje.gd
@@ -2,8 +2,13 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -600.0
 
+
+@onready var animacion = $AnimatedSprite2D
+
+func _process(delta):
+	animacion.play("Idle")
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
